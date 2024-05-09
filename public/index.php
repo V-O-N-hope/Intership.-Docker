e3cde53857949218757219fd9e3eba48ac3c4ce2
Mikita Shidlouski <retdarkw@gmail.com>
2024-05-09 13:27:33 +0300

    update: readme.md updation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hi on third task";
+$dockerServiceHostName = 'mysql'; 
+
+$dockerServiceIP = gethostbyname($dockerServiceHostName);
+
+if ($dockerServiceIP !== $dockerServiceHostName) {
+    echo "Подключение к сети Docker выполнено. IP-адрес сервиса '$dockerServiceHostName' равен: $dockerServiceIP";
+} else {
+    echo "Не удалось подключиться к сети Docker.";
+}
